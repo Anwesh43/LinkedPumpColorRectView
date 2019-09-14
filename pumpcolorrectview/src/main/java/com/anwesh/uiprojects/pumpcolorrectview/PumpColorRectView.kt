9def3419a3035bcbab371ccf4bf7f112d3e0e77a
Anwesh43 <anweshthecool0@gmail.com>
2019-09-14 10:12:32 +0530

    adding Canvas extension methods

--- pumpcolorrectview/src/main/java/com/anwesh/uiprojects/pumpcolorrectview/PumpColorRectView.kt
+++ pumpcolorrectview/src/main/java/com/anwesh/uiprojects/pumpcolorrectview/PumpColorRectView.kt
@@ -25,3 +25,29 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawPumpRect(sc1 : Float, sc2 : Float, size : Float, paint : Paint) {
+    val lw : Float = size / lWFactor
+    val lh : Float = size / lHFactor
+    val ly : Float = (size - lh) * (sc2 - sc1)
+    save()
+    translate(0f, ly)
+    drawRect(RectF(-lw, -lh, lw, 0f), paint)
+    drawLine(0f, 0f, 0f, -ly, paint)
+    restore()
+    drawRect(RectF(-size, 0f, size, size * sc2), paint)
+}
+
+fun Canvas.drawPCRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap * (i + 1), w / 2)
+    drawPumpRect(scale.divideScale(0, 2), scale.divideScale(1, 2), size, paint)
+    restore()
+}
