3d0754e7fcbac3ef4cae5ec49ed8edd31c0098fe
Anwesh43 <anweshthecool0@gmail.com>
2019-09-14 10:02:54 +0530

    adding constants and libraries

--- pumpcolorrectview/src/main/java/com/anwesh/uiprojects/pumpcolorrectview/PumpColorRectView.kt
+++ pumpcolorrectview/src/main/java/com/anwesh/uiprojects/pumpcolorrectview/PumpColorRectView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.pumpcolorrectview
 /**
  * Created by anweshmishra on 14/09/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val scGap : Float = 0.01f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.3f
+val lWFactor : Float = 3f
+val lHFactor : Float = 6.9f
+val foreColor : Int = Color.parseColor("#f44336")
+val backColor : Int = Color.parseColor("#BDBDBD")
