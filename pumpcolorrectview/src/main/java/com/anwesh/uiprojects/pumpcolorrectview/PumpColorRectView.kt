05c7473cf48f1da3bfa9f244328a7c153a5b4181
Anwesh43 <anweshthecool0@gmail.com>
2019-09-14 10:14:09 +0530

    adding PumpColorRectView class

--- pumpcolorrectview/src/main/java/com/anwesh/uiprojects/pumpcolorrectview/PumpColorRectView.kt
+++ pumpcolorrectview/src/main/java/com/anwesh/uiprojects/pumpcolorrectview/PumpColorRectView.kt
@@ -51,3 +51,21 @@ fun Canvas.drawPCRNode(i : Int, scale : Float, paint : Paint) {
     drawPumpRect(scale.divideScale(0, 2), scale.divideScale(1, 2), size, paint)
     restore()
 }
+
+class PumpColorRectView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
