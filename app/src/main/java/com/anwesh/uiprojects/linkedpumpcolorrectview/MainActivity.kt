901c95e74815430e179e20838a6e78e0376c7c09
Anwesh43 <anweshthecool0@gmail.com>
2019-09-15 10:54:11 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedpumpcolorrectview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedpumpcolorrectview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedpumpcolorrectview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.pumpcolorrectview.PumpColorRectView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PumpColorRectView.create(this)
     }
 }
